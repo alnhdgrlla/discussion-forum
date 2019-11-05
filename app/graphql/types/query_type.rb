d5f38430808eae4c98bacbbf92ca6b7bfdb9362a
alnhdgrlla <y.tanaka@shippio.io>
2019-11-05 18:29:48 +0900

    all user resolver added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,5 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     field :all_topics, resolver: Resolvers::AllTopics
+    field :all_users, resolver: Resolvers::AllUsers
   end
 end
