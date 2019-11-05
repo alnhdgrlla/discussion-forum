29fa99a7b14ed020940e573eb31fae983372f14d
alnhdgrlla <y.tanaka@shippio.io>
2019-11-05 18:55:28 +0900

    added validation to delete topic

--- app/graphql/mutations/delete_topic.rb
+++ app/graphql/mutations/delete_topic.rb
@@ -1,3 +1,17 @@
+# module Mutations
+#   class DeleteTopic < BaseMutation
+#     argument :topic_id, ID, required: true
+
+#     type Types::TopicType
+
+#     def resolve(topic_id:)
+#       topic  = Topic.find_by(id: topic_id)
+#       topic.destroy if topic.present?
+#       # return GraphQL::ExecutionError.new("This topic does not exist")
+#     end
+#   end
+# end
+
 module Mutations
   class DeleteTopic < BaseMutation
     argument :topic_id, ID, required: true
@@ -6,8 +20,12 @@ module Mutations
 
     def resolve(topic_id:)
       topic  = Topic.find_by(id: topic_id)
-      topic.destroy if topic.present?
-      # return GraphQL::ExecutionError.new("This topic does not exist")
+      return GraphQL::ExecutionError.new("This topic does not exist") unless topic.present?
+      if topic.user_id == context[:current_user].id
+        topic.destroy
+      else
+        return GraphQL::ExecutionError.new("Forbidden action")
+      end
     end
   end
 end
