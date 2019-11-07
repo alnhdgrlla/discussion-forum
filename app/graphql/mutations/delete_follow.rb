8ab80a2ab4717088c461baad6d4081f09f4c99f8
alnhdgrlla <y.tanaka@shippio.io>
2019-11-07 12:38:49 +0900

    unfollow validation added

--- app/graphql/mutations/delete_follow.rb
+++ app/graphql/mutations/delete_follow.rb
@@ -1,3 +1,29 @@
+# module Mutations
+#   class DeleteFollow < BaseMutation
+
+#     argument :followee_id, ID, required: true
+
+#     field :follower, Types::UserType, null: true
+#     field :followee, Types::UserType, null: true
+#     field :error, String, null: false
+
+#     def resolve(followee_id:)
+#       follower = context[:current_user]
+#       followee = User.find(followee_id) 
+#       if follower.unfollow(followee)
+#         {
+#          follower: follower,
+#          followee: followee
+#         }
+#       else
+#         {
+#         error: GraphQL::ExecutionError.new("cannot unfollow the  same user twice")
+#         }
+#       end
+#     end
+#   end
+# end
+
 module Mutations
   class DeleteFollow < BaseMutation
 
@@ -5,19 +31,20 @@ module Mutations
 
     field :follower, Types::UserType, null: true
     field :followee, Types::UserType, null: true
-    field :error, String, null: false
 
     def resolve(followee_id:)
       follower = context[:current_user]
       followee = User.find(followee_id) 
-      if follower.unfollow(followee)
-        {
-         follower: follower,
-         followee: followee
-        }
+
+      if !follower.followees.include?(followee)
+        return GraphQL::ExecutionError.new("cannot unfollow users that you dont follow ")
+      elsif follower.followees.include?(follower)
+        return GraphQL::ExecutionError.new("cannot unfollow yourself")
       else
+        follower.active_relationships.find_by(followee_id: followee).destroy
         {
-        error: GraphQL::ExecutionError.new("cannot unfollow the  same user twice")
+          follower: follower,
+          followee: followee
         }
       end
     end
