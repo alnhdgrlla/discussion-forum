bc4e246dbf9ad5ebd84b01a172296eaab1189c26
alnhdgrlla <y.tanaka@shippio.io>
2019-11-08 14:44:16 +0900

    added validation on delete topic, reply, favorite

--- app/graphql/mutations/delete_favorite.rb
+++ app/graphql/mutations/delete_favorite.rb
@@ -6,7 +6,7 @@ module Mutations
 
     def resolve(favorite_id:)
       favorite  = Favorite.find_by(id: favorite_id)
-      return GraphQL::ExecutionError.new("No fav") unless favorite.present?
+      return GraphQL::ExecutionError.new("Has not favorited yet") unless favorite.present?
       if favorite.user_id == context[:current_user].id
         favorite.destroy
       else
