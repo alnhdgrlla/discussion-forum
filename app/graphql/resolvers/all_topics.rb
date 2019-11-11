dda3136a1096911cc6104e51d212bdba5bdbbae8
alnhdgrlla <y.tanaka@shippio.io>
2019-11-11 18:37:03 +0900

    added restriction on topic_info

--- app/graphql/resolvers/all_topics.rb
+++ app/graphql/resolvers/all_topics.rb
@@ -1,43 +1,3 @@
-# module Resolvers
-#   class AllTopics < Resolvers::Base
-
-#     type [Types::TopicType], null: false
-
-#     argument :user_id, ID, required: false
-#     argument :limit, Integer, required: false
-#     argument :offset, Integer, required: false
-
-#     def resolve(user_id: nil, limit: 10, offset: 0 )
-#       c_user = context[:current_user]
-#       return GraphQL::ExecutionError.new("You have to log in to favorite") unless c_user.present?
-#       binding.pry
-#       if !user_id
-#         c_feed = c_user.topics
-#         c_feed.limit(limit).offset(offset)
-#         byebug
-#       else
-#         followee = User.find(user_id)
-#         if User.find_by(id: user_id).nil?
-#           return GraphQL::ExecutionError.new("No user exists with that ID")
-#         else
-#           if c_user.followees.include?(followee)  
-#             # topic = Topic.where(user_id: user_id).and(Topic.where(user_id: active_relationships.select(:followee_id)))
-#             topic =Topic.where(user_id: [c_user.id, c_user.followees.select(:id)].flatten)
-            
-#             topic.limit(limit).offset(offset)
-#           else
-#             return GraphQL::ExecutionError.new("You are not following this user")
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-
-
-# all
-# return Tweet.where(user_id: [ cu.id, cu.following.select(:id)].flatten).where("content like ?", "%#{keyword}%")&.order(id: "desc").offset(pagination[0]).limit(pagination[1])
-
 module Resolvers
   class AllTopics < Resolvers::Base
 
@@ -60,7 +20,6 @@ module Resolvers
           return GraphQL::ExecutionError.new("No user exists with that ID")
         else
           if c_user.followees.include?(followee)  
-            # topic = Topic.where(user_id: user_id).and(Topic.where(user_id: active_relationships.select(:followee_id)))
             topic = Topic.where(user_id: followee.id)
             topic
           else
--- app/graphql/resolvers/topic_info.rb
+++ app/graphql/resolvers/topic_info.rb
@@ -1,3 +1,17 @@
+# module Resolvers
+#   class TopicInfo < Resolvers::Base
+#     argument :topic_id, ID, required: true
+
+#     type Types::TopicType, null: false
+
+#     def resolve(topic_id:)
+#       topic = Topic.find_by(id:topic_id)
+#       return GraphQL::ExecutionError.new("This topic does not exist") unless topic
+#       topic
+#     end
+#   end
+# end
+
 module Resolvers
   class TopicInfo < Resolvers::Base
     argument :topic_id, ID, required: true
@@ -5,9 +19,19 @@ module Resolvers
     type Types::TopicType, null: false
 
     def resolve(topic_id:)
+      c_user = context[:current_user]
       topic = Topic.find_by(id:topic_id)
-      return GraphQL::ExecutionError.new("This topic does not exist") unless topic
-      topic
+      poster = User.where(user_id: topic.user_id)
+      
+      if topic.nil?
+        return GraphQL::ExecutionError.new("This topic does not exist")
+      else
+        if !c_user.followees.include?(poster)
+          topic
+        else
+          return GraphQL::ExecutionError.new("You are not following this user")
+        end
+      end
     end
   end
 end
