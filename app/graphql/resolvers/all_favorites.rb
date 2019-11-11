1e4cd6848b4641a8cbd6d577c07584720b812d53
alnhdgrlla <y.tanaka@shippio.io>
2019-11-11 18:41:10 +0900

    added restriction on all_favorites

--- app/graphql/resolvers/all_favorites.rb
+++ app/graphql/resolvers/all_favorites.rb
@@ -1,13 +1,17 @@
 module Resolvers
   class AllFavorites < Resolvers::Base
     argument :user_id, ID, required: true
+    argument :limit, Integer, required: false
+    argument :offset, Integer, required: false
 
     type [Types::FavoriteType], null: false
 
-    def resolve(user_id:)
+    def resolve(user_id:, limit: 10, offset: 0 )
+      c_user = context[:current_user]
       user = User.find_by(id: user_id)
       return GraphQL::ExecutionError.new("this user does not exist") if !user
-      Favorite.where(user_id: user_id) if user
+      return GraphQL::ExecutionError.new("You are not following this user") if !c_user.followees.include?(user)
+      Favorite.where(user_id: user_id).limit(limit).offset(offset)
     end
   end
 end
--- app/graphql/resolvers/topic_info.rb
+++ app/graphql/resolvers/topic_info.rb
@@ -1,17 +1,3 @@
-# module Resolvers
-#   class TopicInfo < Resolvers::Base
-#     argument :topic_id, ID, required: true
-
-#     type Types::TopicType, null: false
-
-#     def resolve(topic_id:)
-#       topic = Topic.find_by(id:topic_id)
-#       return GraphQL::ExecutionError.new("This topic does not exist") unless topic
-#       topic
-#     end
-#   end
-# end
-
 module Resolvers
   class TopicInfo < Resolvers::Base
     argument :topic_id, ID, required: true
